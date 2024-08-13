68f1c1645e0824ceb97a6d60490a798d8ed77dc3
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-08-13 03:16:56 +0200

    Style header

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import Sidebar from "../components/navigation/Sidebar";
 
 const Clients = () => {
-  return <Sidebar>Clients</Sidebar>;
+  return (
+    <Sidebar>
+      <header className="w-full">
+        <h1 className="text-4xl font-semibold text-black-900">Clients</h1>
+      </header>
+    </Sidebar>
+  );
 };
 
 export default Clients;
