12c3ddae924e6a57b472a2db2d433bea004717f8
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-08-13 04:20:14 +0200

    Create clients table

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -11,10 +11,28 @@ const Clients = () => {
   }, []);
   return (
     <Sidebar>
-      <header className="w-full">
+      <header className="w-full flex justify-between items-center">
         <h1 className="text-4xl font-semibold text-black-900">Clients</h1>
+        <div className="flex gap-4 items-center">
+          <button className="h-[3rem] rounded-full px-[1.5rem] bg-primary-900 text-white font-semibold">
+            nouveau client
+          </button>
+        </div>
       </header>
-      <section className="w-full"></section>
+      <section className="w-full mt-4">
+        <table className="w-full">
+          <thead className="w-full">
+            <tr className="w-full h-[3rem] bg-secondary-800 text-white">
+              <th className="w-[10rem] pl-4 text-start">ID</th>
+              <th className="pl-4 text-start">Noms</th>
+              <th className="pl-4 text-start w-[20rem]">telephone</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr></tr>
+          </tbody>
+        </table>
+      </section>
     </Sidebar>
   );
 };
