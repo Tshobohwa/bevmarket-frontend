324d0604988d3f3fb2c90efcc77b8f888fd7a737
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-08-29 08:12:01 +0200

    Fix button

--- src/components/buttons/ButtonHighlight.jsx
+++ src/components/buttons/ButtonHighlight.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const ButtonHighlight = ({ name, onClick }) => {
+const ButtonHighlight = ({ name, onClick, isActive }) => {
   return (
     <button
-      className="h-[3rem] bg-red-900 font-semibold text-white text-xl"
+      className={`h-[3rem] font-semibold text-white text-xl ${
+        !isActive ? "bg-primary-900" : "bg-red-900/20"
+      }`}
       onClick={onClick}
     >
       {name}
--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://bevmarketsaas.onrender.com";
+// export const BASE_URL = "https://bevmarketsaas.onrender.com";
+export const BASE_URL = "http://localhost:3000";
 
 const API_URL = `${BASE_URL}/api/v1`;
 
