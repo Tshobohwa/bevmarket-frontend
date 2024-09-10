c059175b86c5ccfbcb9e6e33505839646b8d7a67
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-09-10 16:19:39 +0200

    Add route

--- src/pages/MyEstablishement.jsx
+++ src/pages/MyEstablishement.jsx
@@ -4,6 +4,7 @@ import AddSalePoint from "../components/popups/AddSalePoint";
 import { IoAdd } from "react-icons/io5";
 import { AiOutlineShop } from "react-icons/ai";
 import { BsShop } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const MyEstablishement = () => {
   const [isAddingSalePoint, setIsAddingSalePoint] = useState(false);
@@ -21,9 +22,11 @@ const MyEstablishement = () => {
       <div className="w-full">
         <h2>Mes points de vente</h2>
         <div className="w-full grid grid-cols-4 gap-4">
-          <div className="w-full bg-white border border-primary-200 p-4 rounded-2xl hover:cursor-pointer hover:shadow-lg">
-            <BsShop size={56} />
-          </div>
+          <Link to={"my-establishement/salepoints/1"}>
+            <div className="w-full h-full bg-white border border-primary-200 p-4 rounded-2xl hover:cursor-pointer hover:shadow-lg">
+              <BsShop size={56} />
+            </div>
+          </Link>
           <div
             className="w-full bg-white border border-primary-200 flex flex-col items-center h-[9rem] justify-center hover:cursor-pointer hover:shadow-lg rounded-2xl font-poppins"
             onClick={() => setIsAddingSalePoint(true)}
