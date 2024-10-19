ca91e0138d220049d8c57577ead96167f479283d
Lucien Makutano <54644582+lucienmakutano@users.noreply.github.com>
2024-10-19 16:47:07 +0000

    add backend url

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://bevmarketsaas.onrender.com";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api-bevmarket-gka0hadhajgph5ba.eastasia-01.azurewebsites.net/";
 
 const API_URL = `${BASE_URL}/api/v1`;
 
