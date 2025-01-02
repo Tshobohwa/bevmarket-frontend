6ad696420864ddd05c2c2a554bff9b374fa9d0e2
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-02 15:05:43 +0200

    Add sale point data

--- src/pages/SalePointStock.jsx
+++ src/pages/SalePointStock.jsx
@@ -9,12 +9,12 @@ import { BiSearch } from "react-icons/bi";
 const SalePointStock = () => {
   const { salePointId } = useParams();
 
-  const { stock } = useSelector((state) => state.stock);
+  const { salePointStock } = useSelector((state) => state.stock);
 
   return (
     <div>
       <SalepointDetailsSideBar salePointId={salePointId} />
-      <div className="ml-[270px] pt-[4rem] mr-4">
+      <div className="ml-[270px] pt-[4rem] mr-4 pl-4">
         <div className="h-[3rem] bg-white right-0 left-[270px] flex items-center justify-end gap-4 px-4 fixed top-0">
           <RoundedInputWithIcon
             placeholder={"Rechercher l'article"}
@@ -23,7 +23,7 @@ const SalePointStock = () => {
           <RoundedButton name={"ajouter au stock"} />
         </div>
 
-        <section className="w-full mt-4 ml-4  bg-white p-4 border border-primary-300 rounded-md">
+        <section className="w-full mt-4 pl-6 bg-white p-4 border border-primary-300 rounded-md">
           <table className="w-full">
             <thead className="w-full">
               <tr className="w-full h-[3rem] text-black-950  border border-primary-100">
@@ -42,6 +42,7 @@ const SalePointStock = () => {
                 <th className="pl-4 text-start  w-[15%]">Quantite</th>
               </tr>
             </thead>
+            <tbody></tbody>
           </table>
         </section>
       </div>
