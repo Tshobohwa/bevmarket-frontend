cea508f6951ee96c88d888f3725e76515c9b0374
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-15 02:18:53 +0200

    Fix partner customer sale

--- src/components/popups/SaleItemQuantityForm.jsx
+++ src/components/popups/SaleItemQuantityForm.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ButtonHighlight from "../buttons/ButtonHighlight";
 import ButtonShadow from "../buttons/ButtonShadow";
 import InputWithLabel from "../Inputs/InputWithLabel";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItemToNewSale } from "../../redux/slices/sales/salesSlice";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
+import { getClient } from "../../redux/slices/clients/clientsActions";
 
 // eslint-disable-next-line react/prop-types
 const SaleItemQuantityForm = ({ currentItem, closeHandler, cancelHandler }) => {
@@ -12,10 +13,17 @@ const SaleItemQuantityForm = ({ currentItem, closeHandler, cancelHandler }) => {
   const [saleQuantity, setSaleQuantity] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
+  const { newSale } = useSelector((store) => store.sales);
+  const { clients } = useSelector((state) => state.clients);
+
+  const [currentClient, setCurrentClient] = useState(
+    clients.find((client) => client.id === newSale.client_id)
+  );
+
   const submitHandler = () => {
     // eslint-disable-next-line react/prop-types
     if (saleQuantity <= 0 || saleQuantity > currentItem.quantity) {
-      toast.error("Verifier la quantité du stock")
+      toast.error("Verifier la quantité du stock");
       return;
     }
 
@@ -25,12 +33,22 @@ const SaleItemQuantityForm = ({ currentItem, closeHandler, cancelHandler }) => {
         // eslint-disable-next-line react/prop-types
         id: currentItem.id,
         // eslint-disable-next-line react/prop-types
-        unit_sale_price: currentItem.unit_sale_price,
+        unit_sale_price: currentClient?.is_partener
+          ? currentItem.reduction_sale_price
+          : currentItem.unit_sale_price,
         quantity: saleQuantity,
       })
-    )
+    );
     closeHandler();
   };
+
+  useEffect(() => {
+    dispatch(getClient());
+  }, []);
+
+  useEffect(() => {
+    setCurrentClient(clients.find((client) => client.id === newSale.client_id));
+  }, [clients]);
 
   return (
     <div className=" absolute top-0 left-0 right-0 bottom-0 bg-black-950/50 flex items-center justify-center backdrop-blur-sm">
@@ -52,7 +70,11 @@ const SaleItemQuantityForm = ({ currentItem, closeHandler, cancelHandler }) => {
         </div>
         <div className="w-full grid grid-cols-2 gap-4">
           <ButtonShadow name={"Annuler"} onClick={cancelHandler} />
-          <ButtonHighlight name={"Ajouter"} onClick={submitHandler} isLoading={isLoading} />
+          <ButtonHighlight
+            name={"Ajouter"}
+            onClick={submitHandler}
+            isLoading={isLoading}
+          />
         </div>
       </div>
     </div>
