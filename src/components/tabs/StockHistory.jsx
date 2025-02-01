fceaad571a7d80c7b0d1ec666df0be46cf7ac938
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-02-01 14:48:46 +0200

    Stock history page

--- src/components/tabs/StockHistory.jsx
+++ src/components/tabs/StockHistory.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const StockHistory = () => {
-  return <div>StockHistory</div>;
+  return (
+    <section className="w-full p-4 rounded-md bg-white border border-secondary-300 mt-4">
+      <div className="w-full flex justify-between items-center"></div>
+    </section>
+  );
 };
 
 export default StockHistory;
