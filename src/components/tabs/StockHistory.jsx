1eb08b4b344c03c9d5744ae9fca6b9ad3637d2bd
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-02-01 15:13:48 +0200

    Add time filter

--- src/components/tabs/StockHistory.jsx
+++ src/components/tabs/StockHistory.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Timefilter from "../filters/Timefilter";
 
 const StockHistory = () => {
+  const [date, setDate] = useState(new Date());
+  const [from, setFrom] = useState(new Date());
+  const [to, setTo] = useState(new Date());
   return (
     <section className="w-full p-4 rounded-md bg-white border border-secondary-300 mt-4">
-      <div className="w-full flex justify-between items-center"></div>
+      <div className="w-full flex justify-between items-center">
+        <div></div>
+        <Timefilter
+          date={date}
+          setDate={setDate}
+          from={from}
+          setFrom={setFrom}
+          to={to}
+          setTo={setTo}
+        />
+      </div>
     </section>
   );
 };
