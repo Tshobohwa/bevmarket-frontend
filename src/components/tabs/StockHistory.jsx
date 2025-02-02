4965651155d5ca2479f5968af2bec991a143f044
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-02-02 12:54:05 +0200

    Render History

--- src/components/tabs/StockHistory.jsx
+++ src/components/tabs/StockHistory.jsx
@@ -31,21 +31,30 @@ const StockHistory = () => {
   }, [stockMovements, stockHistoryFilter]);
 
   useEffect(() => {
-    const reducedStockHistory = {};
+    const newReducedStockHistory = {};
 
     filteredStockHistory.forEach((stockMovement) => {
-      reducedStockHistory[stockMovement.stock_item_id]
-        ? (reducedStockHistory[stockMovement.stock_item_id].quantity +=
-            stockMovement.quantity)
-        : (reducedStockHistory[stockMovement.stock_item_id] = stockMovement);
+      const { stock_item_id, quantity } = stockMovement;
+
+      if (newReducedStockHistory[stock_item_id]) {
+        newReducedStockHistory[stock_item_id] = {
+          ...newReducedStockHistory[stock_item_id],
+          quantity:
+            Number.parseFloat(newReducedStockHistory[stock_item_id].quantity) +
+            Number.parseFloat(quantity),
+        };
+      } else {
+        newReducedStockHistory[stock_item_id] = { ...stockMovement };
+      }
     });
 
-    setReducedStockHistory(reducedStockHistory);
+    setReducedStockHistory(newReducedStockHistory);
   }, [filteredStockHistory]);
 
   useEffect(() => {
     console.log(reducedStockHistory);
   }, [reducedStockHistory]);
+
   return (
     <section className="w-full p-4 rounded-md bg-white border border-secondary-300 mt-4">
       <div className="w-full flex justify-between items-center">
@@ -70,8 +79,30 @@ const StockHistory = () => {
         />
       </div>
       <div className="grid grid-cols-2 w-full mt-4  h-[25rem]">
-        <div>
+        <div className="px-4">
           <p>Resumee</p>
+          <div className=" bg-black-900 text-white py-2 grid grid-cols-2">
+            <p className="pl-2">Article</p>
+            <p className="pl-2">Quanitee</p>
+          </div>
+          {Object.keys(reducedStockHistory).map((key, index) => {
+            const stockItem = reducedStockHistory[key];
+            const item = stockItem.stock_item.item;
+            return (
+              <div
+                className={`grid grid-cols-2 py-2 ${
+                  index % 2 !== 0 ? "bg-primary-100" : ""
+                }`}
+              >
+                <p className="px-2">
+                  {item.name} {item.bottles_number} x {item.capacity} Cl
+                </p>
+                <p className="px-2">
+                  {formatNumber(stockItem.quantity)} Caisses
+                </p>
+              </div>
+            );
+          })}
         </div>
         <div className="h-full overflow-y-scroll">
           <p>Historique</p>
