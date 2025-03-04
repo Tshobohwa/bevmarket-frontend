cb3f240ef93bb1c7273f27f6c2da6380b534f0d4
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-04 09:54:58 +0200

    Reconnect to azur server

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,6 +1,7 @@
 // export const BASE_URL = "https://bevmarketsaas.onrender.com";
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://api-bevmarket-gka0hadhajgph5ba.eastasia-01.azurewebsites.net";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  "https://api-bevmarket-gka0hadhajgph5ba.eastasia-01.azurewebsites.net";
 
 const API_URL = `${BASE_URL}/api/v1`;
 
